abb338ef2206ebb79be275fb2aeec3df2b8fff41
threeset3 <threeset3@github.com>
2015-05-06 18:25:41 +0000

    login callback service

--- js/services.js
+++ js/services.js
@@ -1,26 +1,14 @@
 // js/services/todos.js
 angular.module('demoServices', [])
-    .factory('Location', function($http){
+    .factory('User', function($http){
       return {
-        addr : function(lat,lng) {
+        checkin : function(userID, lat, lng) {
           return $http({
-              method: 'GET',
-              url: 'https://maps.googleapis.com/maps/api/geocode/json' +
-              '?latlng=' + lat + ',' + lng + '&' +
-              '?key=AIzaSyAY0y7uKpLfmfP_XGFeCBYTFxYrjTmtUHE&'
-           });
-        },
-
-        stops : function(lat,lng,num) {
-          return $http({
-              method: 'GET',
-
-              url: 'https://developer.cumtd.com/api/v2.2/json/GetStopsByLatLon' +
-              '?key=a1918356803e4ac5b605b11d3329f0e8' +
-              '&lat=' + lat + '&lon=' + lng + '&count=' + num
+            method: 'POST',
+            url: 'http://localhost:4000/api/user' +
+            '?latlng=' + lat + ',' + lng + '&' +
+            '?userID=' + userID
           });
         }
       }
-
-    })
-    ;
+    });
