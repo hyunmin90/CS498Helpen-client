49650c98a3197d75bfcbab9ac98b46505064b7b2
threeset3 <threeset3@github.com>
2015-05-11 07:32:29 +0000

    fix

--- js/services.js
+++ js/services.js
@@ -99,66 +99,238 @@ var locations = {
         }
     ]
 };
-var classes = {
-    "subject": [
-{Name: "CS 100",    Subject: "Freshman Orientation"},
-{Name: "CS 101",    Subject: "Intro Computing: Engrg & Sci"},
-{Name: "CS 105",    Subject: "Intro Computing: Non-Tech"},
-{Name: "CS 125",    Subject: "Intro to Computer Science"},
-{Name: "CS 173",    Subject: "Discrete Structures"},
-{Name: "CS 196",    Subject: "Freshman Honors"},
-{Name: "CS 199",    Subject: "Undergraduate Open Seminar"},
-{Name: "CS 210",    Subject: "Ethical & Professional Issues"},
-{Name: "CS 225",    Subject: "Data Structures"},
-{Name: "CS 233",    Subject: "Computer Architecture"},
-{Name: "CS 241",    Subject: "System Programming"},
-{Name: "CS 242",    Subject: "Programming Studio"},
-{Name: "CS 296",    Subject: "Honors Course"},
-{Name: "CS 357",    Subject: "Numerical Methods I"},
-{Name: "CS 397",    Subject: "Individual Study"},
-{Name: "CS 411",    Subject: "Database Systems"},
-{Name: "CS 412",    Subject: "Introduction to Data Mining"},
-{Name: "CS 413",    Subject: "Intro to Combinatorics"},
-{Name: "CS 418",    Subject: "Interactive Computer Graphics"},
-{Name: "CS 420",    Subject: "Parallel Progrmg: Sci & Engrg"},
-{Name: "CS 421",    Subject: "Progrmg Languages & Compilers"},
-{Name: "CS 425",    Subject: "Distributed Systems"},
-{Name: "CS 426",    Subject: "Compiler Construction"},
-{Name: "CS 427",    Subject: "Software Engineering I"},
-{Name: "CS 431",    Subject: "Embedded Systems"},
-{Name: "CS 433",    Subject: "Computer System Organization"},
-{Name: "CS 438",    Subject: "Communication Networks"},
-{Name: "CS 439",    Subject: "Wireless Networks"},
-{Name: "CS 440",    Subject: "Artificial Intelligence"},
-{Name: "CS 445",    Subject: "Computational Photography"},
-{Name: "CS 446",    Subject: "Machine Learning"},
-{Name: "CS 447",    Subject: "Natural Language Processing"},
-{Name: "CS 450",    Subject: "Numerical Analysis"},
-{Name: "CS 461",    Subject: "Computer Security I"},
-{Name: "CS 465",    Subject: "User Interface Design"},
-{Name: "CS 476",    Subject: "Program Verification"},
-{Name: "CS 477",    Subject: "Formal Software Devel Methods"},
-{Name: "CS 481",    Subject: "Stochastic Processes & Applic"},
-{Name: "CS 483",    Subject: "Applied Parallel Programming"},
-{Name: "CS 491",    Subject: "Seminar"},
-{Name: "CS 492",    Subject: "Senior Project I"},
-{Name: "CS 498",    Subject: "Special Topics"},
-{Name: "CS 499",    Subject: "Senior Thesis"},
-{Name: "CS 519",    Subject: "Scientific Visualization"},
-{Name: "CS 527",    Subject: "Topics in Software Engineering"},
-{Name: "CS 541",    Subject: "Computer Systems Analysis"},
-{Name: "CS 549",    Subject: "Seminar in Cognitive Science"},
-{Name: "CS 554",    Subject: "Parallel Numerical Algorithms"},
-{Name: "CS 565",    Subject: "Human-Computer Interaction"},
-{Name: "CS 571",    Subject: "Combinatorial Mathematics"},
-{Name: "CS 574",    Subject: "Randomized Algorithms"},
-{Name: "CS 575",    Subject: "Methods of Combinatorics"},
-{Name: "CS 584",    Subject: "Embedded System Verification"},
-{Name: "CS 591",    Subject: "Advanced Seminar"},
-{Name: "CS 597",    Subject: "Individual Study"},
-{Name: "CS 598",    Subject: "Special Topics"},
-{Name: "CS 599",    Subject: "Thesis Research"}
-]};
+var classes = {  
+   "subject":[  
+      {  
+         Name:"CS 100",
+         Subject:"Freshman Orientation"
+      },
+      {  
+         Name:"CS 101",
+         Subject:"Intro Computing: Engrg & Sci"
+      },
+      {  
+         Name:"CS 105",
+         Subject:"Intro Computing: Non-Tech"
+      },
+      {  
+         Name:"CS 125",
+         Subject:"Intro to Computer Science"
+      },
+      {  
+         Name:"CS 173",
+         Subject:"Discrete Structures"
+      },
+      {  
+         Name:"CS 196",
+         Subject:"Freshman Honors"
+      },
+      {  
+         Name:"CS 199",
+         Subject:"Undergraduate Open Seminar"
+      },
+      {  
+         Name:"CS 210",
+         Subject:"Ethical & Professional Issues"
+      },
+      {  
+         Name:"CS 225",
+         Subject:"Data Structures"
+      },
+      {  
+         Name:"CS 233",
+         Subject:"Computer Architecture"
+      },
+      {  
+         Name:"CS 241",
+         Subject:"System Programming"
+      },
+      {  
+         Name:"CS 242",
+         Subject:"Programming Studio"
+      },
+      {  
+         Name:"CS 296",
+         Subject:"Honors Course"
+      },
+      {  
+         Name:"CS 357",
+         Subject:"Numerical Methods I"
+      },
+      {  
+         Name:"CS 397",
+         Subject:"Individual Study"
+      },
+      {  
+         Name:"CS 411",
+         Subject:"Database Systems"
+      },
+      {  
+         Name:"CS 412",
+         Subject:"Introduction to Data Mining"
+      },
+      {  
+         Name:"CS 413",
+         Subject:"Intro to Combinatorics"
+      },
+      {  
+         Name:"CS 418",
+         Subject:"Interactive Computer Graphics"
+      },
+      {  
+         Name:"CS 420",
+         Subject:"Parallel Progrmg: Sci & Engrg"
+      },
+      {  
+         Name:"CS 421",
+         Subject:"Progrmg Languages & Compilers"
+      },
+      {  
+         Name:"CS 425",
+         Subject:"Distributed Systems"
+      },
+      {  
+         Name:"CS 426",
+         Subject:"Compiler Construction"
+      },
+      {  
+         Name:"CS 427",
+         Subject:"Software Engineering I"
+      },
+      {  
+         Name:"CS 431",
+         Subject:"Embedded Systems"
+      },
+      {  
+         Name:"CS 433",
+         Subject:"Computer System Organization"
+      },
+      {  
+         Name:"CS 438",
+         Subject:"Communication Networks"
+      },
+      {  
+         Name:"CS 439",
+         Subject:"Wireless Networks"
+      },
+      {  
+         Name:"CS 440",
+         Subject:"Artificial Intelligence"
+      },
+      {  
+         Name:"CS 445",
+         Subject:"Computational Photography"
+      },
+      {  
+         Name:"CS 446",
+         Subject:"Machine Learning"
+      },
+      {  
+         Name:"CS 447",
+         Subject:"Natural Language Processing"
+      },
+      {  
+         Name:"CS 450",
+         Subject:"Numerical Analysis"
+      },
+      {  
+         Name:"CS 461",
+         Subject:"Computer Security I"
+      },
+      {  
+         Name:"CS 465",
+         Subject:"User Interface Design"
+      },
+      {  
+         Name:"CS 476",
+         Subject:"Program Verification"
+      },
+      {  
+         Name:"CS 477",
+         Subject:"Formal Software Devel Methods"
+      },
+      {  
+         Name:"CS 481",
+         Subject:"Stochastic Processes & Applic"
+      },
+      {  
+         Name:"CS 483",
+         Subject:"Applied Parallel Programming"
+      },
+      {  
+         Name:"CS 491",
+         Subject:"Seminar"
+      },
+      {  
+         Name:"CS 492",
+         Subject:"Senior Project I"
+      },
+      {  
+         Name:"CS 498",
+         Subject:"Special Topics"
+      },
+      {  
+         Name:"CS 499",
+         Subject:"Senior Thesis"
+      },
+      {  
+         Name:"CS 519",
+         Subject:"Scientific Visualization"
+      },
+      {  
+         Name:"CS 527",
+         Subject:"Topics in Software Engineering"
+      },
+      {  
+         Name:"CS 541",
+         Subject:"Computer Systems Analysis"
+      },
+      {  
+         Name:"CS 549",
+         Subject:"Seminar in Cognitive Science"
+      },
+      {  
+         Name:"CS 554",
+         Subject:"Parallel Numerical Algorithms"
+      },
+      {  
+         Name:"CS 565",
+         Subject:"Human-Computer Interaction"
+      },
+      {  
+         Name:"CS 571",
+         Subject:"Combinatorial Mathematics"
+      },
+      {  
+         Name:"CS 574",
+         Subject:"Randomized Algorithms"
+      },
+      {  
+         Name:"CS 575",
+         Subject:"Methods of Combinatorics"
+      },
+      {  
+         Name:"CS 584",
+         Subject:"Embedded System Verification"
+      },
+      {  
+         Name:"CS 591",
+         Subject:"Advanced Seminar"
+      },
+      {  
+         Name:"CS 597",
+         Subject:"Individual Study"
+      },
+      {  
+         Name:"CS 598",
+         Subject:"Special Topics"
+      },
+      {  
+         Name:"CS 599",
+         Subject:"Thesis Research"
+      }
+   ]
+};
 
 angular.module('helpenServices', [])
     .factory('User', function($http){
