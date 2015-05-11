b30b0c7ccf7ba3f921c8b1c58cd0df7d50a137cb
Praful Mehrotra <prafulmehrotra@hotmail.com>
2015-05-10 23:19:58 -0500

    done location

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -107,120 +107,15 @@ helpenControllers.controller('RegisterController', ['$scope', 'User', function($
 }]);
 
 helpenControllers.controller('SubjectController', ['$scope', 'User','Subject', function($scope, User,Subject) {
-  // var user = sessionStorage.getItem('login');
-  // if(user === undefined || user == "" || user == null) {
-  //   window.location.assign("/");
-  // }
-  //$scope.user = user;
+  var user = sessionStorage.getItem('login');
+  if(user === undefined || user == "" || user == null) {
+    window.location.assign("/");
+  }
+  $scope.user = user;
   $scope.subjects = Subject.getSubjects();
   //console.log($scope.subjects);
 
-  var arr =[];
-  var locations = {
-   "places": [
-      {
-         "Name": "Thomas M Siebel Centre for Computer Science",
-         "Lat": 40.113804,
-         "Long": -88.22491,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-
-      },
-      {
-         "Name": "Grainger Engineering Laboratory",
-         "Lat": 40.1125,
-         "Long": -88.22691,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "Digital Computer Laboratory",
-         "Lat": 40.113136,
-         "Long": -88.22666,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "Coordinated Science Laboratory",
-         "Lat": 40.114956,
-         "Long": -88.22672,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "Beckman Institute",
-         "Lat": 40.115803,
-         "Long": -88.227516,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "Electrical and Computer Engineering Building",
-         "Lat": 40.110523,
-         "Long": -88.26953,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "National Centre for Supercomputing Applications",
-         "Lat": 40.114918,
-         "Long": -88.22486,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "Everitt Laboratory",
-         "Lat": 40.110523,
-         "Long": -88.26953,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "Mechanical Engineering Building",
-         "Lat": 40.110996,
-         "Long": -88.22488,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "Talbot Laboratory",
-         "Lat": 40.111862,
-         "Long": -88.228195,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "Engineering hall",
-         "Lat": 40.113834,
-         "Long": -88.227901,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      },
-      {
-         "Name": "Micro and Nanotech Laboratory",
-         "Lat": 40.113834,
-         "Long": -88.2279,
-         "Latituderange":0.000750,
-         "Longituderange":0.000740
-      }
-      ]
-  };
-  var list = locations.places;
-  for(i=0;i<list.length;i++) {
-    //console.log(list[i]);
-    var currlat = 40.1138;
-    var currlongt = -88.2279;
-    var lat = list[i].Lat;
-    var longt = list[i].Long;
-    var latr = list[i].Latituderange;
-    var longtr = list[i].Longituderange;
-    if((lat - latr) <=  currlat && currlat<=(lat + latr)) {
-      if((longt - longtr) <=  currlongt && currlongt<=(longt + longtr)) {
-        arr.push(list[i]);
-      }
-    }
-  }
-  console.log(arr);
+  
 
 
 
@@ -230,12 +125,12 @@ helpenControllers.controller('SubjectController', ['$scope', 'User','Subject', f
 }]);
 
 helpenControllers.controller('FriendController', ['$scope', 'User', function($scope, User) {
-  //var user = sessionStorage.getItem('login');
-  // if(user === undefined || user == "" || user == null) {
-  //   window.location.assign("/");
-  // }
-  //$scope.user = user;
-  //$scope. = Subject.getSubjects();
+  var user = sessionStorage.getItem('login');
+  if(user === undefined || user == "" || user == null) {
+    window.location.assign("/");
+  }
+  $scope.user = user;
+  $scope. = Subject.getSubjects();
   $scope.friends = 
          [{
             "Name": "Hyounimin Wang",
